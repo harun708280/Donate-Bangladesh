1b95fb60281937109f92b46eb3e9e85de150c08f
harunbd80 <harun708280@gmail.com>
2024-09-23 18:29:52 +0600

    home blog toggle add

--- js/feature.js
+++ js/feature.js
@@ -8,3 +8,9 @@ document.getElementById('donation').addEventListener('click',function(){
     showSection('donate-from')
     changeColour('donation')
 })
+
+document.getElementById('blog-btn').addEventListener('click',function(event){
+    event.preventDefault();
+    window.location.href = "./blog.html";
+
+})
